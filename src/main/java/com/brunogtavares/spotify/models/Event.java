702e9e139ae884da9822ebb94597de82a08aac75
bruno78 <devbruno78@gmail.com>
2018-07-06 17:53:40 -0400

    Fix extra column bug

--- src/main/java/com/brunogtavares/spotify/models/Event.java
+++ src/main/java/com/brunogtavares/spotify/models/Event.java
@@ -17,9 +17,6 @@ public class Event {
     @Column(name = "event_description")
     private String eventDescription;
 
-    @Column(name = "event_day")
-    private int eventDay;
-
     @Column(name = "start_time")
     private String startTime;
 
